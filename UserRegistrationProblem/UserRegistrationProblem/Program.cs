58e0c1949c8fec9846932d1c3175a13105d3a43b
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-06-26 20:15:02 +0530

    [Shivaraj] Added Valid Firstname problem

--- UserRegistrationProblem/UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/UserRegistrationProblem/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To User Registration Problems");
+
+using System;
+using System.Collections.Generic;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To User Registration Problems");
+
+            //UC-1
+            ValidFirstName validFirstName = new ValidFirstName();
+            bool validTest1 = validFirstName.validateFirstName("Shivaraj");
+            bool validTest2 = validFirstName.validateFirstName("4Shivaraj");
+            bool validTest3 = validFirstName.validateFirstName("shivaraj");
+            Console.WriteLine("{0} {1} {2}", (validTest1) ? "Valid" : "Invalid", (validTest2) ? "Valid" : "Invalid", (validTest3) ? "Valid" : "Invalid");
+            Console.ReadKey();
+        }
+    }
+}
